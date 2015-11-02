3599331810e5a1af5cf5edf99d09bc990a6efbdd
quentin <quentin.st@gmail.com>
2015-11-02 23:28:45 +0100

    design
    design navbar, liste pizzas et caddie en position fixed

--- pages/views/parts/footer.php
+++ pages/views/parts/footer.php
@@ -1,7 +1,7 @@
-<footer>
-    <p>Site réalisé pour l'examen Framework&amp;POO BES-Dev 2015 - &copy; Quentin Stouffs</p>
-</footer>
+    <footer>
+        <p>Site réalisé pour l'examen Framework&amp;POO BES-Dev 2015 - &copy; Quentin Stouffs</p>
+    </footer>
 
-    
+</div>   
 </body>
 </html>
