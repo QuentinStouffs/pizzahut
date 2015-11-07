891cdd28dcd0f1c3848b8772408c8a310707db46
quentin <quentin.st@gmail.com>
2015-11-07 19:24:28 +0100

    formulaire inscription
    formulaire d'inscription design

--- pages/views/content/login_form_view.php
+++ pages/views/content/login_form_view.php
@@ -1,16 +1,18 @@
-<div id="login_form">
-    <h1>Login</h1>
-    <?= form_open('login/validation');?>
-    <div class="form-group">
-        <?= form_label('pseudo', 'Pseudo'); ?>
-        <?= form_input('pseudo', 'Pseudo', 'class="form-control"'); ?>
+<div id="login_form" class="row">
+   <div class="col-md-6 col-md-offset-3">
+        <h1>Login</h1>
+        <?= form_open('login/validation');?>
+        <div class="form-group">
+            <?= form_label('pseudo', 'Pseudo'); ?>
+            <?= form_input('pseudo', 'Pseudo', 'class="form-control"'); ?>
+        </div>
+        <div class="form-group">
+            <?= form_label('password'); ?>
+            <?= form_password('password', 'Password', 'class="form-control"'); ?>
+        </div>
+        <?= form_submit('submit', 'Login', 'class="btn btn-info"'); ?>
+        <?= anchor('login/inscription', 'Inscription', 'class="btn btn-info"'); ?>  <!--Anchor permet de creer un bouton -->
+        <?= form_close(); ?>
     </div>
-    <div class="form-group">
-        <?= form_label('password'); ?>
-        <?= form_password('password', 'Password', 'class="form-control"'); ?>
-    </div>
-    <?= form_submit('submit', 'Login', 'class="btn btn-info"'); ?>
-    <?= anchor('login/inscription', 'Inscription', 'class="btn btn-info"'); ?>  <!--Anchor permet de creer un bouton -->
-    <?= form_close(); ?>
 
 </div>
